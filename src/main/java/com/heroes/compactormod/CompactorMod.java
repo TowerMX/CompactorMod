328b23cec1a7bc4c0c3fcb1c6f5ba4900d4a18ea
TowerMX <alvarosonic@gmail.com>
2021-05-06 13:52:51 +0200

    Añadidas cosillas del compactor (no está todavía)

--- src/main/java/com/heroes/compactormod/CompactorMod.java
+++ src/main/java/com/heroes/compactormod/CompactorMod.java
@@ -3,7 +3,10 @@ package com.heroes.compactormod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.heroes.compactormod.core.init.BlockInit;
+
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,12 +19,16 @@ public class CompactorMod {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public CompactorMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    	
+    	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    	bus.addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
+    	
+    	BlockInit.BLOCKS.register(bus);
+    	
     }
 
     private void setup(final FMLCommonSetupEvent event) {
     }
 
-//    Más limpito
 }
--- src/main/java/com/heroes/compactormod/core/init/ItemInit.java
+++ src/main/java/com/heroes/compactormod/core/init/ItemInit.java
@@ -1,5 +1,18 @@
 package com.heroes.compactormod.core.init;
 
-public class ItemInit {
+import com.heroes.compactormod.CompactorMod;
 
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit {
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CompactorMod.MOD_ID);
+	
+	public static final RegistryObject<BlockItem> COMPACTOR = ITEMS.register("compactor", () -> new BlockItem(BlockInit.COMPACTOR.get(), new Item.Properties().tab(ItemGroup.TAB_REDSTONE)));
+	
 }
